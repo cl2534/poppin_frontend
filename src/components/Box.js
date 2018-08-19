51e49b661e887a7ddf3a32bc0f6c8301b45f246b
KoichiSakamoto <isaac.fiore@mail.mcgill.ca>
2018-08-19 13:29:33 -0400

    box img

--- src/components/Box.js
+++ src/components/Box.js
@@ -8,7 +8,7 @@ export default class Box extends Component {
 
   render() {
     return (<div className="boxxy">
-      <img src="https://vignette.wikia.nocookie.net/despicableme/images/c/ca/Bob-from-the-minions-movie.jpg/revision/latest?cb=20151224154354" height="200" width="200"/>
+      <img src={this.props.style.style_pic_link} height="200" width="200"/>
       <div className="center-text"> {this.props.style.name} </div>
     </div>)
   }
